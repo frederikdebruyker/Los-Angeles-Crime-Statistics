443470f6fb8101226d5ec511561bea5cae7c0542
Jeff Mackey <jeff.mackey@zoho.com>
2019-03-23 10:36:32 -0500

    added data subset

--- ziptest.py
+++ ziptest.py
@@ -2,4 +2,6 @@ from uszipcode import SearchEngine
 
 search = SearchEngine(simple_zipcode=True)
 result = search.by_coordinates(34.0079, -118.2842, radius=5, returns=1)
-print(result)
+print(f"Zipcode: {result[0].zipcode}")
+print(f"Median Home Value: {result[0].median_home_value}")
+print(f"Median Income: {result[0].median_household_income}")
